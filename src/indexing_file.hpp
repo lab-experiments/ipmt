dd04b97723d3a391006b6390dbdb89924d368190
ThaisaMirely <mirelythaisa@gmail.com>
2015-12-27 11:41:58 -0300

    created index interface

---
+++ src/indexing_file.hpp
@@ -0,0 +1,25 @@
+//
+//  index.hpp
+//  ipmt
+//
+//  Created by Thaisa Mirely.
+//  Copyright © 2015 tmbs. All rights reserved.
+//
+
+#ifndef indexing_file_hpp
+#define indexing_file_hpp
+
+#include <stdio.h>
+#include "command_model.hpp"
+
+class Indexing{
+
+private:
+    
+    
+public:
+    
+    void static IndexingFile(CommandModel command_model);
+
+};
+#endif /* indexing_file_hpp */
