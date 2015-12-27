a542e9d2a46ea00d7ea344e5615cc37369f20c1a
ThaisaMirely <mirelythaisa@gmail.com>
2015-12-27 11:41:42 -0300

    created interface for convert index files

---
+++ src/converted_file.hpp
@@ -0,0 +1,25 @@
+//
+//  converted_file.hpp
+//  ipmt-xcode
+//
+//  Created by Thaisa Mirely on 12/24/15.
+//  Copyright © 2015 tmbs. All rights reserved.
+//
+
+#ifndef converted_file_hpp
+#define converted_file_hpp
+
+#include <stdio.h>
+
+class ConvertedFile{
+
+private:
+    
+    
+    
+public:
+    
+    
+
+};
+#endif /* converted_file_hpp */
